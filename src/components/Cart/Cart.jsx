c43e35660da20eb295f7f86f470b62c31b0c44b5
jemin-balar <balarjemin701@gmail.com>
2023-03-20 17:54:23 +0530

    payment-key includes

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -8,11 +8,32 @@ import { Context } from "../../utils/context";
 import CartItem from "../Cart/CartItem/CartItem";
 
 import "./Cart.scss";
+import { loadStripe } from "@stripe/stripe-js";
+import { makePaymentsRequest } from "../../utils/api";
 
 const Cart = ({ setShowCart }) => {
   const { cartItems, cartSubTotal } = useContext(Context);
+
   const navigate = useNavigate();
 
+  const stripePromise = loadStripe(
+    "pk_test_51MngckSDyJc5gYXqoRe7evJjLfW1uPWiSMjmCW6ls9KE3c1lo8lsMVOhu9LycHzNBmyTnNXtKXek2qVWYkKToUZk00K4PeJVO0"
+  );
+
+  const handlePayment = async () => {
+    try {
+      const stripe = await stripePromise;
+      const res = await makePaymentsRequest.post("/api/orders", {
+        products: cartItems,
+      });
+
+      await stripe.redirectToCheckout({
+        sessionId: res.data.stripeSession.id,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
       <div className="cart-panel">
@@ -41,7 +62,9 @@ const Cart = ({ setShowCart }) => {
                   <span className="text total">&#8377;{cartSubTotal}</span>
                 </div>
                 <div className="button">
-                  <button className="checkout-cta">CheckOut</button>
+                  <button className="checkout-cta" onClick={handlePayment}>
+                    CheckOut
+                  </button>
                 </div>
               </div>
             </>
--- src/utils/api.js
+++ src/utils/api.js
@@ -16,3 +16,12 @@ export const fetchDatFromApi = async (url) => {
     return error;
   }
 };
+
+export const makePaymentsRequest = axios.create({
+  baseURL: "http://localhost:1337",
+  headers: {
+    Authorization:
+      "bearer " +
+      "c6c62a76415294c93edfbe6c07b8e0ebe8358d5d6b469d7b3645af3d128b5d4f7ce1698a332775b1587e2ead55ce699ec47cf1d056d2e87e033c452fe53e1dfe635816bc73c606892c884bb1634e3203df1aebdc615c4f6cd37a9ebd28bcc62cc2deefcfe07c3c36a86f81542a8fb4d33aa941022a75bafcbad9929a8fadf672",
+  },
+});
